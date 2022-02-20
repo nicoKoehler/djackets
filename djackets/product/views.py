e24af354d4954e647cc4752750ff6a837556fff7
nicoKoehler <nk.logins@pm.me>
2022-02-20 16:41:34 +0100

    django: added categories, products, api route

--- djackets/product/views.py
+++ djackets/product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product
+from .serializer import ProductSerializer
+
+class LatestProductsLists(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[0:4] # first 4 products
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
